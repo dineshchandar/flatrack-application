82a6f8e95946cec0a8a8835c83b9d0d5bd231474
Dinesh <dineshchandar@gmail.com>
2025-08-04 18:07:45 +0530

    Update gitignore

---
+++ src/main/java/com/application/flatrack/FlatrackApplication.java
@@ -0,0 +1,13 @@
+package com.application.flatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatrackApplication.class, args);
+	}
+
+}
